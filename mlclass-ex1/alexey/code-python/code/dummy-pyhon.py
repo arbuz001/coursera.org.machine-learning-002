3993b6ef7ec38d1544d10c1e009a204cd62bb9c7
arbuz001 <alexey.arbuzov@gmail.com>
2016-10-02 17:04:16 -0700

    "testing commit from PyCharm"

---
+++ mlclass-ex1/alexey/code-python/code/dummy-pyhon.py
@@ -0,0 +1,11 @@
+import os
+
+# read all files in specified directory and output number of lines
+strFileIn = 'c:/alexey_workspace/sauder/coursera.org/coursera.org.machine-learning-002/mlclass-ex1/alexey/code-python/code/'
+
+for filename in os.listdir(strFileIn):
+	reader = open(strFileIn + filename,'r')
+	lines = reader.readlines()
+	print "::",filename,"-",len(lines),"lines"
+	
+	reader.close()
