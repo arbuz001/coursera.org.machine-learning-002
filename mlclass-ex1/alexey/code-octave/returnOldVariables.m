5fd9d4bfa33a66fe4f7447fdf0780f953df756fb
arbuz001 <alexey.arbuzov@gmail.com>
2016-10-05 11:55:25 -0700

    "week-2, exercise-1: adding raw files and data"

--- mlclass-ex1/alexey/code-octave/returnOldVariables.m
+++
@@ -1,7 +0,0 @@
-function X_old = returnOldVariables(X, mu, sigma)
-%RETURNOLDVARIABLES Normalizes the features in X 
-%   RETURNOLDVARIABLES(X, mu, sigma) returns to old variables by applying inverse of normalization of X
-
-X_old = X .* (ones(size(X,1),1)*sigma) + ones(size(X,1),1)*mu;
-
-end
--- mlclass-ex1/alexey/code-python/code/dummy-pyhon.py
+++ mlclass-ex1/alexey/code-python/code/dummy-pyhon.py
@@ -1,11 +1,37 @@
+'''
+Read all files in specified directory and output number of lines
+'''
+
+import numpy as np
 import os
+import pandas as pd
+import paths
 
-# read all files in specified directory and output number of lines
-strFileIn = 'c:/alexey_workspace/sauder/coursera.org/coursera.org.machine-learning-002/mlclass-ex1/alexey/code-python/code/'
+# strFileIn = 'c:/alexey_workspace/sauder/coursera.org/coursera.org.machine-learning-002/mlclass-ex1/alexey/code-python/data/'
+# # strFileIn = paths.prj_path
 
-for filename in os.listdir(strFileIn):
-	reader = open(strFileIn + filename,'r')
-	lines = reader.readlines()
-	print "::", filename,"-",len(lines),"lines"
+# for filename in os.listdir(strFileIn):
+	# reader = open(strFileIn + filename,'r')
+	# lines = reader.readlines()
+	# print "::", filename,"-",len(lines),"lines"
 	
-	reader.close()
+	# reader.close()
+	
+tol = 1e-5
+
+strFileIn = 'c:/alexey_workspace/sauder/coursera.org/coursera.org.machine-learning-002/mlclass-ex1/alexey/code-python/data/ex1data1.txt'
+
+data = pd.read_csv(strFileIn, sep=',',header=0)
+# data = pd.read_csv(strFileIn)
+
+# X = fnRemoveFeatures(data,['y'])
+
+# g = fnSigmoid(X)
+
+# strFileIn = paths.prj_path + '/utils/test/data-cases/data-case-7.csv'
+
+# out2 = pd.read_csv(strFileIn, sep=',',header=0)
+
+# assert np.amax(abs(g - out2.values)) < tol, "FAILURE in 'fnPredictFunction_test': test case-2 failed!"
+
+print "DONE: test '" + "fnSigmoid_test" + "' completed!";
