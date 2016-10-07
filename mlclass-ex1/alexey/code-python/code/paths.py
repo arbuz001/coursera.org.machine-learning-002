52794958b30cb9440e341b26ad467ceb72740329
arbuz001 <alexey.arbuzov@gmail.com>
2016-10-06 17:49:36 -0700

    week1: assignment 001: adding raw data files

---
+++ mlclass-ex1/alexey/code-python/code/paths.py
@@ -0,0 +1,8 @@
+'''
+Contains paths to data and submission folders
+'''
+import os
+
+# all project foldres consolidated
+prj_path = 'c:/alexey_workspace/sauder/coursera.org/coursera.org.machine-learning-002/mlclass-ex1/alexey/code-python/'
+data_path = os.path.join(prj_path, "/data")
